bbfef80ea47a2c6e7f97adb82712c0338b1b6a9d
vivo8934 <mfundo8934@gmail.com>
2017-05-23 10:18:40 +0200

    adding handlebars to the route of greeted

--- index.js
+++ index.js
@@ -1,24 +1,67 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
-var greeted = [];
+//var greeted = [];
+//var greetCount = [];
+var counterMap = {};
 
 const app = express();
 
-app.get('/greetings/:name', function(req, res){
-var name = (req.params.name.substr(0,1).toUpperCase() + '' +  req.params.name.substr(1).toLowerCase());
-greeted.push(name);
-res.send('Hello, '  + name);
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
+var upperCaseName = function(name){
+  var name = (name.substr(0, 1).toUpperCase() + '' + name.substr(1).toLowerCase());
+return name;
+}
+
+app.get('/greetings/:name', function(req, res) {
+
+  var name = upperCaseName(req.params.name);
+
+  if (counterMap[name] === undefined) {
+    counterMap[name] = 0;
+  }
+  counterMap[name]++;
+
+  res.send('Hello, ' + name);
+
+  // var foundName = greeted.find(function(currentName) {
+  //   return currentName === name;
+  // });
+  // if (!foundName) {
+  //   greeted.push(name);
+  // }
+  //greetCount.push(name);
+  //console.log(greetCount);
 });
-app.get('/greeted', function(req, res){
 
-  res.send(greeted);
+app.get('/greeted', function(req, res) {
+  // get all the names greeted
+  var greeted = [];
+
+  //create a list from all the Key Objects - the key us the names
+  for(name in counterMap){
+    greeted.push(name);
+  }
+
+  res.render('greetings/index.handlebars', {greetings: greeted});
 })
+
+app.get('/counter/:name', function(req, res) {
+
+  const name = upperCaseName(req.params.name);
+
+  //have I greeted this name  before?
+
+  res.send('Hello ' + name + " " + 'has been greeted ' + counterMap[name] + " " + 'time(s)');
+})
+
 
 const port = 3000;
 
-app.listen(port, function(){
+app.listen(port, function() {
 
-console.log('port number is: ' + port);
+  console.log('port number is: ' + port);
 
 })
