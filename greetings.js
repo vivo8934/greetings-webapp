961c363d48a12e3daa569acc27c59e6566c25ce6
vivo8934 <mfundo8934@gmail.com>
2017-08-10 16:08:30 +0200

    added radio buttons

--- greetings.js
+++ greetings.js
@@ -11,31 +11,24 @@ const getMenu = function(req, res){
 // create a massage for greeting
 const add = function(req, res){
   var name = req.body.name;
+  name = name.substr(0,1).toUpperCase() +  name.substr(1).toLowerCase();
+  var radioBtn = req.body.languages;
 
-  var foundName = greetedList.find(function(currentName){
-       return currentName === name;
-     });
-if(!name){
-req.flash('error', 'Name should not be blank');
-
+  // var foundName = greetedList.find(function(currentName){
+  //      return currentName === name;
+    //  });
+if(!name || !radioBtn){
+req.flash('error', 'enter name or select radio button');
 }
-else {
-    if(!foundName){
-      greetedList.push(name);
-      req.flash('massage');
-    }
-    else {
-     req.flash('error', 'Name already exists!');
-   }
-}
-if(counterMap[name] === undefined){
+else if(counterMap[name] === undefined){
     counterMap[name] = 0;
   }
   counterMap[name] ++;
   const greetedCounter = counterMap[name]
-  var msg = 'Hello' + ' ' + name.substr(0,1).toUpperCase() + name.substr(1).toLowerCase();
+    var msg = radioBtn + ' ' + name;
 res.render('greetings/add', {massage: msg});
 }
+
 
 const index = function(req, res){
   res.render('greetings/index', {greetings : greetedList});
@@ -43,8 +36,8 @@ const index = function(req, res){
 
   const counter = function(req,res){
 
-  var name = req.params.name;
-  console.log(counterMap[name]);
+  var name = req.body.name;
+//  console.log(counterMap[name]);
 const greetedCounter = counterMap[name];
   res.send("Hello,"+ " " + name + " " +"has been greeted" +" " + greetedCounter +" "+"time(s)")
 
